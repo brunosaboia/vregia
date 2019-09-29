1dcd5d112f860733944aaef8d2d67f5b80d917e9
Joao Gomes <jlfgomes@gmail.com>
2019-09-30 00:14:51 +0200

    FIx avatar

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,7 @@ import { HeaderContent } from "../content/i18nContent";
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: "none" } : {}}>
+    <div className="avatar"></div>
     <div className="content">
       <div className="inner">
         <Logo />
